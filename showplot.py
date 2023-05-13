bba2c1b463473c6e9e3ae1c453deb46328dab20b
ReDiGermany <support@redigermany.de>
2023-05-13 09:08:54 +0200

    Added area filter

--- showplot.py
+++ showplot.py
@@ -1,21 +1,29 @@
 import glob
+import sys
 import numpy as np
 import open3d as o3d
 
 from utils.parseFile import getParsedFiles
 
+basedir = "data"
+
+if len(sys.argv)!=10:
+    basedir = sys.argv[1]
 
 pcd = o3d.geometry.PointCloud()
 vis = o3d.visualization.Visualizer()
 vis.create_window()
 
-dir = glob.glob("data\\raw\\*.point")
+dir = glob.glob(f"{basedir}\\*.point")
 # Sort by filename like data\\raw\\123-133.point - windows shows 0,1,11,12,13,2,21 etc.
 dir.sort(key=lambda el: int(el.split(".")[0].split("-")[1]))
 
-files = getParsedFiles(dir)
+visible_area = [[-30,-200,-100],[30,200,100]]
 
-for items in files:
+files = getParsedFiles(dir,visible_area)
+
+def show(items):
+
     pcd.points = o3d.utility.Vector3dVector(items)
     pcd.rotate(pcd.get_rotation_matrix_from_xyz((1.5 * np.pi, 0, 1.1 * np.pi)), center=(0,0,0))
     opt = vis.get_render_option()
@@ -24,3 +32,6 @@ for items in files:
     vis.poll_events()
     vis.update_renderer()
     vis.run()
+
+for items in files:
+    show(items)
--- utils/parseFile.py
+++ utils/parseFile.py
@@ -1,4 +1,18 @@
-def parseFile(name):
+def isVisible(item,visible_area):
+    return item < visible_area[1] and item > visible_area[0]
+
+def inVisibleArea(item,visible_area):
+    ret = item[0] > visible_area[0][0] and\
+          item[0] < visible_area[1][0] and\
+          \
+          item[1] > visible_area[0][1] and\
+          item[1] < visible_area[1][1] and\
+          \
+          item[2] > visible_area[0][2] and\
+          item[2] < visible_area[1][2]
+    return ret
+
+def parseFile(name,visible_area=[]):
     items = []
 
     with open(name) as file:
@@ -9,12 +23,14 @@ def parseFile(name):
                 l = line.split(" ")
                 for idx,n in enumerate(l):
                     l[idx] = int(n)
-                items.append(l)
+                if inVisibleArea(l,visible_area):
+                    items.append(l)
     return items
 
-def getParsedFiles(dir):
+def getParsedFiles(dir,visible_area=[]):
     files = []
     for file in dir:
-        data = parseFile(file)
+        data = parseFile(file,visible_area)
+        
         files.append(data)
     return files
