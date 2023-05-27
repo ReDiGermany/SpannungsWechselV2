fe9e3f0fa0b5801ef209473f598d1bc1efff679c
ReDiGermany <support@redigermany.de>
2023-05-27 20:11:43 +0200

    Added log view

--- utils/parseFile.py
+++ utils/parseFile.py
@@ -22,14 +22,15 @@ def parseFile(name,visible_area=[]):
             for line in lines:
                 l = line.split(" ")
                 for idx,n in enumerate(l):
-                    l[idx] = int(n)
+                    l[idx] = float(n)
                 if inVisibleArea(l,visible_area):
                     items.append(l)
     return items
 
 def getParsedFiles(dir,visible_area=[]):
     files = []
-    for file in dir:
+    for idx,file in enumerate(dir):
+        print(idx*100/len(dir))
         data = parseFile(file,visible_area)
         
         files.append(data)
