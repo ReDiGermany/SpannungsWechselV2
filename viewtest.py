d28db7924fdcc65a0641e08385571f8f4ce81735
ReDiGermany <support@redigermany.de>
2023-05-31 17:38:21 +0200

    progress

--- viewtest.py
+++ viewtest.py
@@ -1,20 +1,38 @@
 import glob
-import math
-import random
 import time
-import jsonpickle
 import numpy as np
 import open3d as o3d
-from statistics import mode
-
 from utils.parseFile import  parseFile
+import datetime
 
-# dir = glob.glob("C:\\Users\\suppo\\Documents\\spannungswechsel logs\\logs-1685189669\\logs\\*.xyz")
-dir = glob.glob("G:\\spannungswechsel\\logs\\logs-1685194131\\logs\\*.xyz")
+# Configs
+folder = "C:\\Users\\suppo\\Documents\\spannungswechsel logs\\logs-1685189669"
+folder = "C:\\Users\\suppo\\Documents\\spannungswechsel logs\\logs-1685194131"
+# folder = "C:\\Users\\suppo\\Documents\\spannungswechsel logs"
+# folder = "G:\\spannungswechsel\\logs\\logs-1685194131"
+line_color = 1
+grid_color = 1
+z_padding = 1
+
+print(f"Welcome!\nFinding all coordinates inside: {folder}")
+
+dir = glob.glob(f"{folder}\\logs\\*.xyz")
+
+
+timestamps = []
+for item in dir:
+    item = item.split("\\")
+    item = item[len(item)-1]
+    timestamps.append(int(item.split(".")[0]))
+timestamps.sort()
+total_seconds = timestamps[len(timestamps)-1]-timestamps[0]
+print(f"Found {len(timestamps)} Logs with a total time of: {str(datetime.timedelta(seconds=total_seconds))} Seconds ({round(len(timestamps)/total_seconds,2)} Logs/Second)")
 
 pcd = o3d.geometry.PointCloud()
 vis = o3d.visualization.Visualizer()
 vis.create_window()
+vis.set_full_screen(True)
+
 
 visible_area = [[-30,-200,-100],[30,200,100]]
 
@@ -29,7 +47,7 @@ def show(items):
     pcd.points = o3d.utility.Vector3dVector(itms)
     pcd.colors = o3d.utility.Vector3dVector(colors)
     pcd.rotate(pcd.get_rotation_matrix_from_xyz((1.6 * np.pi, 0, 0)), center=(0,0,0))
-    pcd.rotate(pcd.get_rotation_matrix_from_xyz((0, 0, 0)), center=(0,0,0))
+    # pcd.rotate(pcd.get_rotation_matrix_from_xyz((0, 0, 0)), center=(0,0,0))
     opt = vis.get_render_option()
     opt.background_color = np.asarray([0, 0, 0])
     vis.add_geometry(pcd)
@@ -61,7 +79,7 @@ def dings(data):
 
 
         # if x > -3 and x < 3 and y < 10 and y > 1.5 and z < 0:
-        if x > -3*4 and x < 3*4 and y < 10*4:
+        # if x > -3*4 and x < 3*4 and y < 10*4:
         # if x > -3*4 and x < 3*4 and y < 10*4 and y > 1.5*4 and z < 0:
             # print(z)
             # p = [x*4,y*4,round(z*4,2),c]
@@ -83,21 +101,55 @@ def dings(data):
 
     show(arr)
 
+# https://im-coder.com/wie-berechne-ich-den-schnittpunkt-zweier-linien-in-python.html
+def line_intersection(line1, line2):
+    xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
+    ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
 
-def doDings(dir,n):
-    print(n,dir[n])
-    data = parseFile(dir[n],visible_area)
-    print(len(data))
+    def det(a, b):
+        return a[0] * b[1] - a[1] * b[0]
 
-    updated_data = []
+    div = det(xdiff, ydiff)
+    if div == 0:
+       raise Exception('lines do not intersect')
+
+    d = (det(*line1), det(*line2))
+    x = det(d, xdiff) / div
+    y = det(d, ydiff) / div
+    return x, y
+
+def getZFunction(x_min,x_max,y):
+    # https://math.stackexchange.com/questions/28043/finding-the-z-value-on-a-plane-with-x-y-values
+    a1=x_min[0]
+    a2=y[0]
+    a3=x_max[0]
+
+    b1=x_min[1]
+    b2=y[1]
+    b3=x_max[1]
+
+    c1=x_min[2]
+    c2=y[2]
+    c3=x_max[2]
+        
+    v1 = np.array([a1-a2,b1-b2,c1-c2])
+    v2 = np.array([a1-a3,b1-b3,c1-c3])
+    r,s,t = np.cross(v1,v2)
+    # print(n)
+    
+    def zfn(x,y):
+        return (1/t)*(r*a1+s*b1+t*c1-r*x-s*y)
+    return zfn
+    
+def getZfn(data):
+    temp = []
     for x,y,z in data:
         # if x > -3 and x < 3 and y < 10 and y > 1.5 and z < 0:
         if x > -3*4*100 and x < 3*4*100 and y < 10*4*100:
         # if x > -3*4 and x < 3*4 and y < 10*4 and y > 1.5*4 and z < 0:
-            p = [x,y,z]
-            updated_data.append(p)
-    data = updated_data
-    print(len(data))
+            temp.append([x,y,z])
+    data = temp
+    # print(len(data))
 
     x_min = [999999,999999,999999]
     x_max = [-999999,-999999,-999999]
@@ -120,76 +172,105 @@ def doDings(dir,n):
             z_min = item
         if item[2] > z_max[2]:
             z_max = item
-    
-    print(f"x_min={x_min}")
-    print(f"x_max={x_max}")
 
-    print(f"y_min={y_min}")
-    print(f"y_max={y_max}")
-
-    print(f"z_min={z_min}")
-    print(f"z_max={z_max}")
-
-
-    print(len(data))
-    # print("data[0] =",data[0])
-    z_idx = int(np.argmin(np.var(data, axis = 0)))
-    # print("z_idx =",z_idx)
-    # data_z = data[:,z_idx]
-    temp = []
-    for n in data:
-        # if n[z_idx] < 0:
-        temp.append(n[z_idx])
-
-    data_z = np.array(temp).astype(int)
-    # data_z = np.array([n[z_idx] for n in data]).astype(int)
-    # print("data_z =",data_z)
-    mean = np.mean(data_z)
-    # print("mean =",mean)
-    sd = np.std(data_z)
-    # print("sd =",sd)
-    padding = 0.3
-    # print(f"(padding * sd) = {(padding * sd)}")
-    # print(f"(mean + padding * sd) = {(mean + padding * sd)}")
-    # print("padding =",padding)
-    data_ground = []
-    data_wo_ground = []
+    y_first = data[0]
+    y_last = [0,0,0]
     for x,y,z in data:
-        test = (z < (mean + padding * sd)) and (z > (mean - padding * sd))
-        c = .5 if test else .5
-        data_ground.append([x,y,z,c])
-        # else:
-            # data_wo_ground.append([x,y,z])
-    # for itm in range(100):
-    itm = 2
-    data_ground[itm][3] = 1
-    data_ground[itm][2] += 100
-    print(itm)
-    itm = round(len(data_ground)/2)
-    data_ground[itm][3] = 1
-    data_ground[itm][2] += 100
-    print(itm)
-    # print(data_ground[10])
-    # data_ground[3][2] = 100
+        if y > y_last[1]:
+            y_last = [x,y,z]
+        else:
+            break
 
-    # print("len(data_ground) =",len(data_ground))
-    arr = []
-    for x,y,z,c in data_ground:
-        arr.append([x/100,y/100,z/100,c])
-    # print(arr)
-    print(len(arr))
-    dings(arr)
+    intersection = line_intersection((x_min, x_max), (y_first, y_last))
+    print(intersection)
 
-    # print("len(data_wo_ground) =",len(data_wo_ground))
-    # ydat = []
-    # for item in data:
-    #     ydat.append([item[0],item[1],item[2],1])
-    # dings(ydat)
+    # https://math.stackexchange.com/questions/28043/finding-the-z-value-on-a-plane-with-x-y-values
+    a1=x_min[0]
+    a2=y_last[0]
+    a3=x_max[0]
+
+    b1=x_min[1]
+    b2=y_last[1]
+    b3=x_max[1]
+
+    c1=x_min[2]
+    c2=y_last[2]
+    c3=x_max[2]
+        
+    v1 = np.array([a1-a2,b1-b2,c1-c2])
+    v2 = np.array([a1-a3,b1-b3,c1-c3])
+    r,s,t = np.cross(v1,v2)
+    # print(n)
+    k = r*a1+s*b1+t*c1
+    
+    def zfn(x,y):
+        return (1/t)*(r*a1+s*b1+t*c1-r*x-s*y)
+    return zfn,x_min,x_max,y_first,y_last,intersection
+
+def doDings(dir,n,fn):
+    zfn,x_min,x_max,y_first,y_last,intersection = fn
+    print(n,dir[n])
+    data = parseFile(dir[n],visible_area)
+    # print(len(data))
+
+    updated_data = []
+    for x,y,z in data:
+        # if x > -3 and x < 3 and y < 10 and y > 1.5 and z < 0:
+        if x > -3*4*100 and x < 3*4*100 and y < 10*4*100:
+        # if x > -3*4 and x < 3*4 and y < 10*4 and y > 1.5*4 and z < 0:
+            p = [x,y,z]
+            updated_data.append(p)
+    data = updated_data
+    # print(len(data))
+
+    # k = 
+    temp = []
+    for x,y,z in data:
+        zz = zfn(x,y)+z_padding
+        # print(f"z={z} zz={zz}")
+        if z > zz:
+            temp.append([x,y,z,1])
+        else:
+            temp.append([x,y,z,.5])
+    data = temp
+
+    def getPercentageValueInInterval(min,percentage,max):
+        return (percentage * (max - min) / 100) + min
+
+    def getPercentageValuesInIntervals(min,percentage,max):
+        x = getPercentageValueInInterval(min[0],percentage,max[0])
+        y = getPercentageValueInInterval(min[1],percentage,max[1])
+        z = getPercentageValueInInterval(min[2],percentage,max[2])
+        return x,y,z
+
+    # print(x_min,x_max)
+    l = range(x_min[0],x_max[0])
+    l_len = len(l)
+    for idx,x in enumerate(l):
+        x,y,z = getPercentageValuesInIntervals(x_min,idx*100/l_len,x_max)
+        data.append([x,y,z,line_color])
+    data.append([x_min[0],x_min[1],x_min[2]+10,line_color])
+    data.append([intersection[0],intersection[1],10,line_color])
+
+    l = range(y_first[1],y_last[1])
+    l_len = len(l)
+    for idx,x in enumerate(l):
+        x,y,z = getPercentageValuesInIntervals(y_first,idx*100/l_len,y_last)
+        data.append([x,y,z+z_padding,line_color])
+    
+    # for x in range(x_min[0],x_max[0],50):
+    #     for y in range(y_first[1],y_last[1],50):
+    #         data.append([x,y,zfn(x,y)+z_padding,grid_color])
+
+    dings(data)
+
+dir = dir[100:]
+zfn = getZfn(parseFile(dir[0],visible_area))
 
 # for idx,file in enumerate(dir):
-#     doDings(dir,idx)
+    # doDings(dir,idx,zfn)
 
 # idx = random.randint(0,len(dir)-1)
 idx = 91
 
-doDings(dir,idx)
+doDings(dir,idx,zfn)
